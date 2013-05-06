bd5ed20369fc70704de076301b358e68ed098444
Matt Lovan <magrelo404@gmail.com>
2013-05-05 20:30:29 -0700

    Layout and Links

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,14 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1', controller: MyCtrl1});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2', controller: MyCtrl2});
+    //$routeProvider.when('/view1', {templateUrl: 'partials/partial1', controller: MyCtrl1});
+    //$routeProvider.when('/view2', {templateUrl: 'partials/partial2', controller: MyCtrl2});
+    $routeProvider.when('/Bio', {templateUrl: 'partials/Bio', controller: MyCtrl1});
+    $routeProvider.when('/Projects', {templateUrl: 'partials/Projects', controller: MyCtrl1});
+    $routeProvider.when('/Blog', {templateUrl: 'partials/Blog', controller: MyCtrl1});
+    $routeProvider.when('/Resume', {templateUrl: 'partials/Resume', controller: MyCtrl1});
+    $routeProvider.when('/Sandbox', {templateUrl: 'partials/Sandbox', controller: MyCtrl1});
+
     $routeProvider.otherwise({redirectTo: '/view1'});
     $locationProvider.html5Mode(true);
   }]);
