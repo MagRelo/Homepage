eafd6e5a1be9ec3a54cca306f1fe182b2660ce49
Matt Lovan <magrelo404@gmail.com>
2013-05-12 09:35:02 -0700

    add lowercase routes

--- public/js/app.js
+++ public/js/app.js
@@ -5,8 +5,11 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.when('/Bio', {templateUrl: 'partials/Bio'});
+    $routeProvider.when('/bio', {templateUrl: 'partials/Bio'});
     $routeProvider.when('/Projects', {templateUrl: 'partials/Projects'});
+    $routeProvider.when('/projects', {templateUrl: 'partials/Projects'});
     $routeProvider.when('/Resume', {templateUrl: 'partials/Resume', controller: ResumeCtrl});
+    $routeProvider.when('/resume', {templateUrl: 'partials/Resume', controller: ResumeCtrl});
     $routeProvider.otherwise({redirectTo: '/Bio'});
     $locationProvider.html5Mode(true);
   }]);
